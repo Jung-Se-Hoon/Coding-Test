074643e875910cc92aabda161ccc96ea9b297ef9
정세훈 <117325026+Jung-Se-Hoon@users.noreply.github.com>
2023-07-16 15:12:33 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,7 +1,6 @@
 a = int(input())
 
-for i in range(1, 2*a):
-    if i < a+1:
-        print(" " * (a-i) + i * '*' + (i -1) * '*')
-    else:
-        print(" " * (i-a) + (((2*a -1)-i) * 2 + 1) * '*' )
+for i in range(1, a):
+    print(' ' * (a-i) + (2*i-1) * '*')
+for i in range(a, 0, -1):
+    print(' ' * (a-i) + (2*i-1) * '*')
