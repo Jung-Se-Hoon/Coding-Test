342bc92b26b9e6c3c484000c2f1eef8f88f17b4b
정세훈 <117325026+Jung-Se-Hoon@users.noreply.github.com>
2024-09-02 10:20:33 +0900

    [Bronze III] Title: 알람 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,11 +1,12 @@
 H, M = map(int, input().split())
 
-if M < 45 :	
-    if H == 0 :	
-        H = 23
-        M += 60
-    else :	
-        H -= 1	
-        M += 60
-        
-print(H, M-45)	
+M -= 45
+
+if M < 0:
+    H -= 1
+    M += 60
+
+if H == -1:
+    H = 23
+
+print(H, M)
